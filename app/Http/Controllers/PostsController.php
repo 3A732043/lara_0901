c8d21e2138509f105223f42fc496c168d66b2637
Wu Chen-En <ap9941002@gmail.com>
2020-12-01 18:40:40 +0800

    更正練習3-1:修改AdminPostsController-使用Post.php(model)查詢資料(改錯Controller)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,15 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
-
 class PostsController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('created_at','DESC')->get();
-        $data=['posts'=>$posts];
-        return view('admin.posts.index',$data);
+        return view('posts.index');
     }
 
     public function show($id)
